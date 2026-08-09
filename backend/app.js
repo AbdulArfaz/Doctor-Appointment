3bb07df0a03c5bd3f02dcd4ef4d874b966f26d23
AbdulArfaz <abdularfaz00@gmail.com>
2026-08-09 17:13:59 +0530

    impliment admin cors policy

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,9 @@ import userRouter from "./routes/user.route.js"
 const app = express()
 
 app.use(cookieParser())
+
 app.use(cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type",'aToken', "Authorization","adminToken"]
@@ -19,7 +20,6 @@ app.use(express.urlencoded({extended: true,limit: "15mb"}))
 
 
 app.use('/api/admin', adminRouter)
-//localhost:4000/api/admin
 
 app.use('/api/doctors', doctorRouter)
 
