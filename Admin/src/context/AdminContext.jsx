7156f6cf7bf08df2edf7e0658707494ae941c0f5
AbdulArfaz <abdularfaz00@gmail.com>
2026-08-17 03:56:29 +0530

    continuing get all appointments in admin dashboard

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -1,53 +1,83 @@
 import axios from "axios";
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 import { toast } from "react-toastify";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
-const AdminContextProvider = ( props )=>{
-    const [doctorslist, setDoctorslist] = useState([])
+const AdminContextProvider = (props) => {
+  const [doctorslist, setDoctorslist] = useState([]);
+ const [appointments,setAppointments] = useState([])
+  const [adminToken, setAdminToken] = useState(
+    localStorage.getItem("accessToken") || "",
+  );
 
- const[adminToken, setAdminToken] = useState(localStorage.getItem('accessToken') || '');
- 
- const backendurl = import.meta.env.VITE_BACKEND_URL
+  const backendurl = import.meta.env.VITE_BACKEND_URL;
 
- const getAllDoctorsList = async () => {
+  const getAllDoctorsList = async () => {
     try {
-        const {data} =await axios.post(backendurl + '/api/admin/all-doctors', {}, {headers: {adminToken}})
+      const { data } = await axios.post(
+        backendurl + "/api/admin/all-doctors",
+        {},
+        { headers: { adminToken } },
+      );
+      if (data.success) {
+        setDoctorslist(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        backendurl + "/api/admin/change-availability",
+        { docId },
+        { headers: { aToken: adminToken } },
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctorsList();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  const getAllAppointments = async () =>{
+    try {
+        const { data }= await axios.get(`${backendurl}/api/admin/appointments`,
+            {headers:{aToken: adminToken}}
+        )
         if(data.success){
-            setDoctorslist(data.data)
-            // console.log(data);
-                        
-        }else{
+            setAppointments(data.appointments)
+        } else {
             toast.error(data.message)
         }
     } catch (error) {
         toast.error(error.message)
     }
- }
+  }
 
- const changeAvailability = async (docId) => {
-    try {
-        const {data} = await axios.post(backendurl + '/api/admin/change-availability', {docId}, {headers: {aToken : adminToken}})
-        if (data.success) {
-            toast.success(data.message)
-            getAllDoctorsList()
-        }else{
-            toast.error(data.message)
-        }
-    } catch (error) {
-        toast.error(error.message)
-    }
- }
-
-    const value = {
-          adminToken,setAdminToken,
-          backendurl,doctorslist,getAllDoctorsList,changeAvailability,
-    }
-    return(
-        <AdminContext.Provider value={value}>
-        {props.children}
-        </AdminContext.Provider>
-    )
-}
-export default AdminContextProvider
+  const value = {
+    adminToken,
+    setAdminToken,
+    backendurl,
+    doctorslist,
+    getAllDoctorsList,
+    changeAvailability,
+    appointments,
+    setAppointments,
+    getAllAppointments,
+  };
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+export default AdminContextProvider;
--- Admin/src/pages/AdminAllAppointments.jsx
+++ Admin/src/pages/AdminAllAppointments.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import { useEffect } from 'react'
 
 const AdminAllAppointments = () => {
+
+const { adminToken, appointments, getAllAppointments} = useContext(AdminContext)
+
+useEffect(()=>{
+if (adminToken) {
+  getAllAppointments()
+}
+},[adminToken])
+
   return (
     <div>
-      
+        <p>All Appointments</p>
+        <div>
+          <div>
+            <p>#</p>
+            <p>Patient</p>
+            <p>Age</p>
+            <p>Date & Time</p>
+            <p>Doctor</p>
+            <p>Fees</p>
+            <p>Action</p>
+          </div>
+
+          {appointments.map((item,index)=>(
+              <div key={index}>
+                <p>{index + 1}</p>
+                <div>
+                  <img src={item.userData.image} alt='' />
+                  <p>{item.userData.name}</p>
+                </div>
+              </div>
+          ))
+          }
+        </div>
     </div>
   )
 }
