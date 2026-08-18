6dd8aec6856d71bdffb9c40e7eeb80fe19e18807
AbdulArfaz <abdularfaz00@gmail.com>
2026-08-18 05:32:30 +0530

    started the doctor UI part

--- Admin/src/context/DoctorContext.jsx
+++ Admin/src/context/DoctorContext.jsx
@@ -1,26 +1,49 @@
-import { useState } from "react"
-import { createContext } from "react"
+import { useState } from "react";
+import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export const DoctorContext = createContext()
+export const DoctorContext = createContext();
 
-const DoctorContextProvider = ( props )=>{
+const DoctorContextProvider = (props) => {
+  const backendurl = import.meta.env.VITE_BACKEND_URL;
 
-    const backendurl = import.meta.env.VITE_BACKEND_URL
+  const [docToken, setDocToken] = useState(
+    localStorage.getItem("docToken") || "",
+  );
+  const [appointments, setAppointments] = useState([]);
 
-    const [docToken, setDocToken] = useState(
-       localStorage.getItem("docToken") || "",
-    )
-
-
-    const value = {
-      docToken,
-      setDocToken,
-      backendurl,
+  const getAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        `${backendurl}/api/doctors/appointments`,
+        { headers: { Authorization: `Bearer ${docToken}` } },
+      );
+      
+      if (data.success) {
+        const reversedList = [...(data.data || [])].reverse()
+        setAppointments(reversedList);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data?.message);
     }
-    return(
-        <DoctorContext.Provider value={value}>
-        {props.children}
-        </DoctorContext.Provider>
-    )
-}
-export default DoctorContextProvider
+  };
+
+  const value = {
+    docToken,
+    setDocToken,
+    backendurl,
+    appointments,
+    setAppointments,
+    getAppointments,
+  };
+  return (
+    <DoctorContext.Provider value={value}>
+      {props.children}
+    </DoctorContext.Provider>
+  );
+};
+export default DoctorContextProvider;
--- Admin/src/pages/Doctor/DoctorAppointments.jsx
+++ Admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
+import { useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { useEffect } from 'react'
 
 const DoctorAppointments = () => {
+
+const { docToken, appointments, getAppointments} = useContext(DoctorContext)
+
+useEffect(()=>{
+if (docToken) {
+  getAppointments()
+}
+},[docToken])
+
   return (
     <div>
-      DoctorAppointments
-    </div>
-  )
-}
+      <p>All Appointments</p>
+      <div>
+            <p>#</p>
+            <p>Patient</p>
+            <p>Payment</p>
+            <p>Age</p>
+            <p>Date & Time</p>
+            <p>Fees</p>
+            <p>Action</p>
+          </div>
 
+          { appointments.map((item,index)=>(
+              <div key={index}>
+                   <p>{index + 1}</p>
+              <img src={item.userData.image} alt='' />
+              <p>{item.userData.name}</p>
+              <div>
+                <p>
+                  {item.payment ? 'Online' : 'Cash'}
+                </p>
+              </div>
+              <p></p>
+              </div>
+            ))
+          }
+     </div>
+  )}
+     
+ 
 export default DoctorAppointments
